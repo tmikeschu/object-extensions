03affa80c32c4103d7ad8908e508ada8b9f25b16
Mike Schutte <schutte@quikly.com>
2017-11-24 12:41:25 -0700

    Update build

--- dist/bundle.js
+++ dist/bundle.js
@@ -79,7 +79,20 @@ __webpack_require__(1);
 "use strict";
 
 
-__webpack_require__(2);
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _objectExtensions = __webpack_require__(2);
+
+Object.defineProperty(exports, "Object", {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_objectExtensions).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /***/ }),
 /* 2 */
@@ -88,9 +101,12 @@ __webpack_require__(2);
 "use strict";
 
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-// fn(key: Any, value: Any): Object
 Object.prototype.map = function (fn) {
   var _this = this;
 
@@ -106,6 +122,8 @@ Object.prototype.reduce = function (fn, initial) {
     return fn(acc, x, _this2[x]);
   }, initial);
 };
+
+exports.default = Object;
 
 /***/ })
 /******/ ]);
