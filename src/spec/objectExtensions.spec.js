7e952dab1fb369436872b6b81fba91f04dcd6838
Mike Schutte <schutte@quikly.com>
2017-11-24 12:18:53 -0700

    Add unit test for Object.prototype.map

--- src/spec/objectExtensions.spec.js
+++ src/spec/objectExtensions.spec.js
@@ -2,7 +2,17 @@ import { Object } from '../lib'
 
 describe('Object.prototype extensions', () => {
   describe('.map', () => {
-    it('', () => {
+    it('maps a function over each key value pair', () => {
+      const mapper = (key, value) => ({
+        [key.toUpperCase()]: value * value
+      })
+
+      const ages = { mike: 26, hannah: 25 }
+
+      expect(ages.map(mapper)).toEqual({
+        MIKE: 676,
+        HANNAH: 625,
+      })
     });
   });
 });
