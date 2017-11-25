e625034b98bde8e7a4aa6521e2e423549b1c4941
Mike Schutte <schutte@quikly.com>
2017-11-25 09:30:03 -0700

    Specify function return type for .map

--- src/lib/objectExtensions.js
+++ src/lib/objectExtensions.js
@@ -1,6 +1,11 @@
 Object.prototype.map = function(fn) {
   return Object.keys(this)
-    .reduce((acc, x) => ({ ...acc, ...fn(x, this[x]) }), {})
+    .reduce((acc, x) => {
+      const mapped = fn(x, this[x])
+      const valid = typeof mapped === "object" && !Array.isArray(mapped)
+      if (!valid) { throw "Function must return an object {}" }
+      return { ...acc, ...mapped }
+    }, {})
 }
 
 Object.prototype.reduce = function(fn, initial) {
--- src/spec/objectExtensions.spec.js
+++ src/spec/objectExtensions.spec.js
@@ -1,5 +1,7 @@
 import { Object } from '../lib'
 
+const { map, reduce } = Object.prototype
+
 describe('Object.prototype extensions', () => {
   describe('.map', () => {
     it('maps a function over each key value pair', () => {
@@ -9,10 +11,25 @@ describe('Object.prototype extensions', () => {
 
       const ages = { mike: 26, hannah: 25 }
 
-      expect(ages.map(mapper)).toEqual({
+      expect(map.bind(ages)(mapper)).toEqual({
         MIKE: 676,
         HANNAH: 625,
       })
-    });
-  });
-});
+    })
+
+    it('only accepts a function that returns an object', () => {
+      const mapPrimitive = (key, value) => key.toUpperCase()
+      const mapArray = (key, value) => ([key, value])
+
+      const ages = { mike: 26, hannah: 25 }
+
+      expect(() => {
+        map.bind(ages)(mapPrimitive)
+      }).toThrow("Function must return an object {}")
+
+      expect(() => {
+        map.bind(ages)(mapArray)
+      }).toThrow("Function must return an object {}")
+    })
+  })
+})
